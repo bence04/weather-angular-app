596db55174c54df8bce16936c009afad2805923b
bence04 <tothbence95@gmail.com>
2018-09-16 23:14:04 +0200

    added cities delete function

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -8,11 +8,22 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class CardComponent implements OnInit {
 
   @Input() cities: any[];
+  @Output() citiesChange = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
     console.log(this.cities);
+  }
+
+  removeCity(cityName) {
+    const newCities = [];
+    Object.keys(this.cities).forEach(key => {
+      if (this.cities[key].cityName !== cityName) {
+        newCities.push(this.cities[key]);
+      }
+    });
+    this.citiesChange.emit(newCities);
   }
 
 }
