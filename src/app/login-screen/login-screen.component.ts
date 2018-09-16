001b7808854145537a7109a776df35032bb905b5
bence04 <tothbence95@gmail.com>
2018-09-16 21:07:50 +0200

    finish login and registration

--- src/app/login-screen/login-screen.component.ts
+++ src/app/login-screen/login-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-screen',
@@ -6,19 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-screen.component.scss']
 })
 export class LoginScreenComponent implements OnInit {
-
   user = {
+    id: 0,
     name: '',
     password: ''
   };
+  users = [];
+  loginError = false;
+  inputError = false;
 
-  constructor() { }
+  constructor(public router: Router) {
+    if (localStorage.getItem('users')) {
+      this.users = JSON.parse(localStorage.getItem('users'));
+    }
+  }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  checkInputs() {
+    if (this.user.name === '' || this.user.password === '') {
+      this.inputError = true;
+      this.loginError = false;
+      return false;
+    }
+    this.inputError = false;
+    return true;
   }
 
   loginBtnClick() {
-    console.log(this.user);
-  }
+    if (this.checkInputs()) {
+      let founded = false;
+      Object.keys(this.users).forEach(key => {
+        if (this.users[key].name === this.user.name) {
+          founded = true;
+          if (this.users[key].password === this.user.password) {
+            localStorage.setItem('logged', '1');
+            this.router.navigate(['home']);
+          } else {
+            this.loginError = true;
+          }
+        }
+      });
 
+      if (!founded) {
+        this.user.id = this.users.length;
+        this.users.push(this.user);
+        localStorage.setItem('users', JSON.stringify(this.users));
+        localStorage.setItem('logged', '1');
+        this.router.navigate(['home']);
+      }
+    }
+  }
 }
